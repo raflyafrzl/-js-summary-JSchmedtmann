fa8ef45783aad4d00cf98cbe6719f445fe2facc4
raflyafrzl <comfortyourstep@gmail.com>
2022-01-29 15:29:31 +0700

    Add Nullish Coalescing

---
+++ DS-MODERN-OP-STRINGS/NullishCoalescing.js
@@ -0,0 +1,56 @@
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  orderDelivery: function ({
+    address = null,
+    time,
+    starterIndex,
+    mainIndex,
+    namaPelanggan = null,
+  }) {
+    if (address === null || namaPelanggan === null) {
+      alert("Anda tidak memasukan alamat/Nama");
+    } else {
+      console.log(
+        `Halo ${namaPelanggan}, anda memesan ${this.starterMenu[starterIndex]} dan ${this.mainMenu[mainIndex]}  pada jam ${time} dengan alamat ${address}`
+      );
+    }
+  },
+  orderPasta: function (ing, ing2, ing3) {
+    console.log(`Here is your pasta with ${ing}, ${ing2}, and ${ing3}`);
+  },
+
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    if (otherIngredients.length >= 1) console.log(otherIngredients);
+  },
+};
+
+//Nullish -> sama seperti Short Circuiting OR. hanya saja dia mengambil nilai null dan undefined
+// berbeda dengan OR yaitu 0 dan ' '
+
+restaurant.numGuest = 0;
+const guest = restaurant.numGuest ?? 5;
+
+console.log(guest); // -> hasilnya adalah 0

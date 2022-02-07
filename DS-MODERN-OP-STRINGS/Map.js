a859d17d0aa9bafc8f185600547669c3ff8ee332
raflyafrzl <comfortyourstep@gmail.com>
2022-02-07 23:44:34 +0700

    Add Map

---
+++ DS-MODERN-OP-STRINGS/Map.js
@@ -0,0 +1,22 @@
+//Map
+
+//Struktur Data seperti Sets, hanya saja dia memiliki pasangan key value pair serta key nya boleh selain String
+
+const rest = new Map();
+
+//Memasukan Nilai ke Map
+rest.set("Nama", "Muhammad Rafly Afrizal Pratama");
+
+rest.set(true, "Dia Laki Laki");
+rest.set(123, "Bilangan ");
+
+console.log(rest.has(true));
+console.log(rest.get(123));
+
+const map = new Map();
+//urutan dalam Map juga seperti Set, yaitu irrelevant
+map.set(1, "Angka Pertama");
+map.set(3, "Angka Ketiga");
+map.set(2, "Angka kedua");
+
+console.log(map);

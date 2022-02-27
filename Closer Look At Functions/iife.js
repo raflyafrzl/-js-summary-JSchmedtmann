a547eecb4ddf3ac7338c1a3b472a6946e32430fe
raflyafrzl <comfortyourstep@gmail.com>
2022-02-27 23:11:26 +0700

    Add IIFE

---
+++ Closer Look At Functions/iife.js
@@ -0,0 +1,7 @@
+//IIFE
+//Function that we called below here would be executed once
+//it's because IIFE, we immediately called function statement as function expression
+
+(function () {
+  console.log("test");
+})();

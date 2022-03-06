c43a15706f3d192556806b473ad2dfaa9ea9b26d
raflyafrzl <comfortyourstep@gmail.com>
2022-03-06 20:03:06 +0700

    Add Comment for closure

--- Closer Look At Functions/Closure.js
+++ Closer Look At Functions/Closure.js
@@ -1,3 +1,8 @@
+//closure membuat sebuah function mengingat variable yang pernah ada pada function awalnya/atasnya
+//Closure mengingat variable dari parent function. jadi meskipun parent function telah
+//me return sebuah function, function return tersebut akan tetap dapat mengakses VE milik
+//parent function.
+
 const secureBooking = function () {
   let test = 0;
 
@@ -10,4 +15,4 @@ const secureBooking = function () {
 const booker = secureBooking();
 
 //namun saat pemanggilan disini, variable test masih bisa digunakan
-booker();
+console.dir(secureBooking());

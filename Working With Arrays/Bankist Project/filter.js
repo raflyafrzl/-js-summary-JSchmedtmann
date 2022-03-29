c0676366f5b3ac602a5a7b65c67a08fd0bde0e98
raflyafrzl <comfortyourstep@gmail.com>
2022-03-30 00:34:07 +0700

    Add Filter

--- Working With Arrays/Bankist Project/filter.js
+++ Working With Arrays/Bankist Project/filter.js
@@ -1,1 +1,9 @@
-const test = 'rafly';
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(el => el > 0);
+console.log(movements);
+console.log(deposits);
+
+const withdrawals = movements.filter(el => el < 0);
+
+console.log(withdrawals);
